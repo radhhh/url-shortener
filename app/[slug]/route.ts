19199996779f25d2e2370c8c1d9efd97a2be9270
Radhya Cahya Kusuma <123277320+radhhh@users.noreply.github.com>
2025-11-27 14:24:46 +0700

    added nodejs runtime declaration to fix prisma bug

--- app/[slug]/route.ts
+++ app/[slug]/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { createHash } from "crypto";
 
+export const runtime = "nodejs";
+
 export async function GET(
   req: NextRequest,
   { params }: { params: Promise<{ slug: string }> },
--- app/api/links/route.ts
+++ app/api/links/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { linkFormSchema } from "@/lib/schema";
 
+export const runtime = "nodejs";
+
 export async function POST(req: NextRequest) {
   try {
     const json = await req.json();
