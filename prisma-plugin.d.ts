cbd29e5bfe50b92ef6976b34eff34607be180905
Radhya Cahya Kusuma <123277320+radhhh@users.noreply.github.com>
2025-11-28 20:17:48 +0700

    added binaryTargets in schema.prisma to solve deployment bug

--- prisma-plugin.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "@prisma/nextjs-monorepo-workaround-plugin" {
-  import type { WebpackPluginInstance, Compiler } from "webpack";
-
-  export class PrismaPlugin implements WebpackPluginInstance {
-    apply(compiler: Compiler): void;
-  }
-}
